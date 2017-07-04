8c63142de5efdd7f3d7151dbb8db95d3ca538e0d
Sebastian Kylander <sebastian@kylander.me>
2017-07-05 00:50:28 +0200

    Add UE4 framework

---
+++ Source/GameJam/GameJamGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GameJamGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_API AGameJamGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
